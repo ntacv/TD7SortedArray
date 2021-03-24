11b58640d41f28e02687a331b97b7d5d7b21a72a
ntacv <ntackn1@gmail.com>
2021-03-24 13:20:08 +0100

    First try to the sorting algorithm

--- Program.cs
+++ Program.cs
@@ -48,16 +48,76 @@ namespace TD7SortedArray
                 }
             }
         }
+        static int[] GenererTableauAleatoire(int taille, int valeurMin, int valeurMax)
+        {
+            int[] tableau;
+            if (taille > 0)
+            {
+                tableau = new int[taille];
+                Random random = new Random();
+                for (int i = 0; i < taille; i++)
+                {
 
+                    int number = random.Next(valeurMin, valeurMax + 1);
+                    tableau[i] = number;
+                }
+            }
+            else
+            {
+                tableau = null;
+            }
+            return tableau;
+        }
 
+        static bool Permuter(int[] tableau, int index1, int index2)
+        {
+            bool possible = false;
+            if (tableau != null && tableau.Length != 0 && tableau.Length > index1 && tableau.Length > index2)
+            {
 
+                int temp = tableau[index1];
+                tableau[index1] = tableau[index2];
+                tableau[index2] = temp;
 
+                possible = true;
+            }
+            return possible;
+        }
+        static bool SortingArray(int[] tableau)
+        {
+            bool sorted = false;
+
+            if (tableau != null && tableau.Length != 0)
+            {
+                bool allSorted = false;
+                do
+                {
+                    for (int i = 0; i < tableau.Length-1; i++)
+                    {
+                        if (tableau[i] >= tableau[i + 1])
+                        {
+                            Permuter(tableau, tableau[i], tableau[i + 1]);
+
+                        }
+                    }
+                } while (!allSorted);
+                sorted = true;
+            }
+            return sorted;
+        }
         static void Main(string[] args)
         {
-            
+
+            int[] tableau = GenererTableauAleatoire(5, 0, 10);
+            AfficherTableau(tableau);
+
+            SortingArray(tableau);
+            AfficherTableau(tableau);
 
 
+            Console.ReadKey();
 
         }
     }
 }
+
