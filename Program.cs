56981ae0a9a5d91e26e99db791e41be40bba6511
ntacv <ntackn1@gmail.com>
2021-03-24 12:39:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TD7SortedArray
+{
+    class Program
+    {
+        static void AfficherTableau(int[] tableau)
+        {
+            if (tableau == null)
+            {
+                Console.WriteLine("tableau null");
+            }
+            else
+            {
+                if (tableau.Length == 0)
+                {
+                    Console.WriteLine("tableau vide");
+                }
+                else
+                {
+                    for (int i = 0; i < tableau.Length - 1; i++)
+                    {
+                        Console.Write(tableau[i] + " ; ");
+                    }
+                    Console.WriteLine("" + tableau[tableau.Length - 1] + "");
+                }
+            }
+        }
+        static void AfficherTableauDouble(double[] tableau)
+        {
+            if (tableau == null)
+            {
+                Console.WriteLine("tableau null");
+            }
+            else
+            {
+                if (tableau.Length == 0)
+                {
+                    Console.WriteLine("tableau vide");
+                }
+                else
+                {
+                    for (int i = 0; i < tableau.Length - 1; i++)
+                    {
+                        Console.Write(tableau[i] + " ; ");
+                    }
+                    Console.WriteLine("" + tableau[tableau.Length - 1] + "");
+                }
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
